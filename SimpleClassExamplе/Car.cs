94872975d49e7b4b310ef293e003b50a3e280885
Максим <tomich7372@gmail.com>
2018-02-01 15:17:14 +0700

    adsd

---
+++ SimpleClassExamplе/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExamplе
+{
+    class Car
+    {
+        //Состояние объекта Car
+        public string petName;
+        public int currSpeed;
+
+        public void PrintState()
+            //PrintState - выводит текущее состояние объекта Car в окно командной строки
+        {
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+        public void SpeedUp(int delta)
+            // Увеличивает скорость Car, увеличимвая на величину, переданную во входящем параметре типа int
+        {
+            currSpeed += delta;
+        }
+    }
+
+    
+
+}
---
+++ SimpleClassExamplе/Shkolnik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExamplе
+{
+    class Shkolnik
+    {
+        public int shkolnikSpeed;
+        public string shkolnikName;
+
+        //Связываем конструктор в цепочку
+        public Shkolnik() { }
+        public Shkolnik(int speed) 
+            :this(speed, "") { }
+        public Shkolnik(string name) 
+            :this(0, name) { }
+        //Главный конструктор, использующий необязательные аргументы
+        public Shkolnik(int speed = 1, string name = "Сережа")
+        {
+            if (speed > 5)
+            {
+                speed = 5;
+            }
+
+            shkolnikSpeed = speed;
+            shkolnikName = name;
+        }
+    }
+}
